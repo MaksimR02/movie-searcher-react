814f2e95323d2742ec79fdcf058e7b79b8a95ecb
Reshetniak Maksim <resetnak32@gmail.com>
2024-04-22 20:39:33 +0400

    feat: fixed PR comments

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import Header from './components/header/Header.jsx';
+import Body from './components/body/Body.jsx';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+}
